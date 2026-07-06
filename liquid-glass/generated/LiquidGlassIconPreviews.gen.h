238144a42c7d5ea9b7287ec5040d79bb60bec671
ep39egqa <132845378+ep39egqa@users.noreply.github.com>
2026-07-05 23:50:57 -0400

    Add Helios Cryo icons and simplify Helios asset IDs

--- liquid-glass/generated/LiquidGlassIconPreviews.gen.h
+++ liquid-glass/generated/LiquidGlassIconPreviews.gen.h
@@ -82,16 +82,18 @@ static const LGIconRowEntry kLGGroupEntries_custom[] = {
     { "bajader-glitched", "Glitched", "bajader" },
     { "paulo1manso-modern", "Modern", "paulo1manso" },
     { "paulo1manso-modern-alt", "Modern Alt", "paulo1manso" },
-    { "IllIIllIllIllII-helios", "Helios", "IllIIllIllIllII" },
-    { "IllIIllIllIllII-helios-alt", "Helios Halo", "IllIIllIllIllII" },
-    { "IllIIllIllIllII-helios-ultra", "Helios Ultra", "IllIIllIllIllII" },
-    { "IllIIllIllIllII-helios-ultra-alt", "Helios Ultra Halo", "IllIIllIllIllII" },
+    { "helios", "Helios", "IllIIllIllIllII" },
+    { "helios-halo", "Helios Halo", "IllIIllIllIllII" },
+    { "helios-cryo", "Helios Cryo", "IllIIllIllIllII" },
+    { "helios-cryo-halo", "Helios Cryo Halo", "IllIIllIllIllII" },
+    { "helios-ultra", "Helios Ultra", "IllIIllIllIllII" },
+    { "helios-ultra-halo", "Helios Ultra Halo", "IllIIllIllIllII" },
 };
 
 static const LGIconGroupDef kLGIconGroups[] = {
     { "community", "Liquid Glass", LGGroupPresentationInline, kLGGroupEntries_community, 4 },
     { "new-variants", "New Variants", LGGroupPresentationPush, kLGGroupEntries_new_variants, 45 },
-    { "custom", "Custom", LGGroupPresentationPush, kLGGroupEntries_custom, 9 },
+    { "custom", "Custom", LGGroupPresentationPush, kLGGroupEntries_custom, 11 },
 };
 
 static const size_t kLGIconGroupCount = 3;
